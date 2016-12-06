00fd4fb9ef97abe8624904bb3b7bb3142f918859
kachoungho <kachoungho@naver.com>
2016-12-06 10:39:52 +0900

    no message

---
+++ src/main/webapp/sql/InsertP2_daymoney.sql
@@ -0,0 +1,74 @@
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 10000000, '야탑', '2016', '01', '31', '2016-01-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 9000000, '야탑', '2016', '02', '28', '2016-02-28');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 8000000, '야탑', '2016', '03', '31', '2016-03-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 11000000, '야탑', '2016', '04', '30', '2016-04-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 12000000, '야탑', '2016', '05', '31', '2016-05-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 8000000, '야탑', '2016', '06', '30', '2016-06-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 7000000, '야탑', '2016', '07', '31', '2016-07-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 6000000, '야탑', '2016', '08', '31', '2016-08-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 12000000, '야탑', '2016', '09', '30', '2016-09-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 10000000, '야탑', '2016', '10', '31', '2016-10-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 11000000, '야탑', '2016', '11', '30', '2016-11-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 10000000, '야탑', '2016', '12', '31', '2016-12-31');
+
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 9000000, '정자', '2016', '01', '31', '2016-01-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 11000000, '정자', '2016', '02', '28', '2016-02-28');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 12000000, '정자', '2016', '03', '31', '2016-03-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 11000000, '정자', '2016', '04', '30', '2016-04-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 13000000, '정자', '2016', '05', '31', '2016-05-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 8000000, '정자', '2016', '06', '30', '2016-06-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 12000000, '정자', '2016', '07', '31', '2016-07-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 10000000, '정자', '2016', '08', '31', '2016-08-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 11000000, '정자', '2016', '09', '30', '2016-09-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 7000000, '정자', '2016', '10', '31', '2016-10-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 8000000, '정자', '2016', '11', '30', '2016-11-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 10000000, '정자', '2016', '12', '31', '2016-12-31');
+
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 15000000, '판교', '2016', '01', '31', '2016-01-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 14000000, '판교', '2016', '02', '28', '2016-02-28');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 11000000, '판교', '2016', '03', '31', '2016-03-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 10000000, '판교', '2016', '04', '30', '2016-04-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 9000000, '판교', '2016', '05', '31', '2016-05-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 13000000, '판교', '2016', '06', '30', '2016-06-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 12000000, '판교', '2016', '07', '31', '2016-07-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 17000000, '판교', '2016', '08', '31', '2016-08-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 15000000, '판교', '2016', '09', '30', '2016-09-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 11000000, '판교', '2016', '10', '31', '2016-10-31');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 13000000, '판교', '2016', '11', '30', '2016-11-30');
+insert into p2_daymoney(bno, title, content, pay, area, year, month, days, dayda)
+values(p2_daymoney_num.nextVal, '판매', '수입', 15000000, '판교', '2016', '12', '31', '2016-12-31');
